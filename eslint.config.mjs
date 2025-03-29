92dbe509576abca49b9796da858dadfc6646055f
Eric Hester <eric.hester@umbrella.tech>
2025-03-29 18:12:50 -0400

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,40 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import parser from '@typescript-eslint/parser';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: parser,
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-    },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+eslintConfig.push(
+  compat.config({
     rules: {
+      "react/react-in-jsx-scope": "off",
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_' },
       ],
+      "import/no-anonymous-default-export": [
+        "error",
+        {
+          allowArray: true,
+          allowArrowFunction: true,
+          allowAnonymousClass: true,
+          allowAnonymousFunction: true,
+          allowCallExpression: true,
+          allowLiteral: true,
+          allowObject: true,
+        },
+      ],
     },
-  },
-];
+  }),
+);
+
+export default eslintConfig;
